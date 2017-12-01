a6789e17aa58145bd7d21bf1b21f9a22e596cf48
Alexis Jacomy <alexis.jacomy@gmail.com>
2017-12-01 11:14:39 +0100

    Loading config and data into app

--- src/state.js
+++ src/state.js
@@ -1,14 +1,30 @@
+import _ from 'lodash';
 import Baobab from 'baobab';
+
+const timeSeries = {
+  news: require('../assets/data/timeseries_candidates_news.json'),
+  web: require('../assets/data/timeseries_candidates_web.json'),
+  youtube: require('../assets/data/timeseries_candidates_youtube.json'),
+};
+
+const config = require('../assets/config.json');
 
 export default new Baobab({
   nav: {
-    candidates: {},
-    sources: {},
+    sources: _(config.sources)
+      .keyBy('id')
+      .mapValues(() => true)
+      .value(),
+    candidates: _(config.candidates)
+      .keyBy('id')
+      .mapValues(() => true)
+      .value(),
     options: {
       flatten: false,
     },
   },
   data: {
-    timeSeries: {},
+    config,
+    timeSeries,
   },
 });
